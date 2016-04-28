7026846a4f6da53e3d6d27fb6da1415c7eb449d1
chenuin <yuyin0036@gmail.com>
2016-04-27 21:36:28 -0700

    DHCPServer tab modified

--- server.py
+++ server.py
@@ -3,15 +3,15 @@ from sys import stderr
 import binascii
 
 def convertBytes(value, length):
-	#print(value)
-	strValue = hex(value)
-	strValue = strValue[2:]
-	while len(strValue) < length*2 :
-		strValue = '0' + strValue
-	valueBytes = b''
-	valueBytes = binascii.unhexlify(strValue)
-	#print(valueBytes)
-	return valueBytes
+    #print(value)
+    strValue = hex(value)
+    strValue = strValue[2:]
+    while len(strValue) < length*2 :
+        strValue = '0' + strValue
+    valueBytes = b''
+    valueBytes = binascii.unhexlify(strValue)
+    #print(valueBytes)
+    return valueBytes
 
 class DHCPDiscover:
     def __init__(self, data):
@@ -33,116 +33,116 @@ class DHCPDiscover:
             print(' {0:20s} : {1:15s}'.format(key[i], val[i]))
 
 class DHCPOffer:
-	def __init__(self, data, offerIP, nextServerIP, subnetMask, router, leaseTime, DHCPServer, DNS1, DNS2, DNS3):
-	    self.xid = data.xid
-	    self.mac = data.mac
-	    self.offerIP = offerIP
-	    self.nextServerIP = nextServerIP
-	    self.subnetMask = subnetMask
-	    self.router = router
-	    self.leaseTime = leaseTime
-	    self.server = DHCPServer
-	    self.DNS1 = DNS1
-	    self.DNS2 = DNS2
-	    self.DNS3 = DNS3
-	    
-	    #print(self.xid)
-	    #print(self.mac)
-	def sendPacket(self):
-	    packet = b''
-	    packet += b'\x02'   #Message type: Boot Request (1)
-	    packet += b'\x01'   #Hardware type: Ethernet
-	    packet += b'\x06'   #Hardware address length: 6
-	    packet += b'\x00'   #Hops: 0
-	    packet += self.xid
-	    packet += b'\x00\x00'    #Seconds elapsed: 0
-	    packet += b'\x80\x00'    #Bootp flags: 0x8000 (Broadcast) + reserved flags
-	    packet += inet_aton('0.0.0.0')
-	    packet += inet_aton(self.offerIP)
-	    packet += inet_aton(self.nextServerIP)
-	    packet += inet_aton('0.0.0.0')
-	    packet += self.mac
-	    packet += b'\x00' * 10
-	    packet += b'\x00' * 192
-	    packet += b'\x63\x82\x53\x63' #Magic Cookie
-	    packet += b'\x35\x01\x02'     # Message Type(Option 53 len=1 type=2(offer))
-	    packet += b'\x01\x04' #subnet mask
-	    packet += inet_aton(self.subnetMask) 
-	    packet += b'\x03\x04' #router
-	    packet += inet_aton(self.router) #router
-	    packet += b'\x33\x04' #lease time
-	    packet += convertBytes(self.leaseTime,4)
-	    packet += b'\x36\x04' #DHCP server
-	    packet += inet_aton(self.server)
-	    packet += b'\x07\x04' #DNS servers
-	    packet += inet_aton(self.DNS1)
-	    packet +=  b'\x07\x04' #DNS servers
-	    packet +=  inet_aton(self.DNS2)
-	    packet += b'\x07\x04' #DNS servers
-	    packet += inet_aton(self.DNS3)
-	    
-	    return bytes(packet)
+    def __init__(self, data, offerIP, nextServerIP, subnetMask, router, leaseTime, DHCPServer, DNS1, DNS2, DNS3):
+        self.xid = data.xid
+        self.mac = data.mac
+        self.offerIP = offerIP
+        self.nextServerIP = nextServerIP
+        self.subnetMask = subnetMask
+        self.router = router
+        self.leaseTime = leaseTime
+        self.server = DHCPServer
+        self.DNS1 = DNS1
+        self.DNS2 = DNS2
+        self.DNS3 = DNS3
+        
+        #print(self.xid)
+        #print(self.mac)
+    def sendPacket(self):
+        packet = b''
+        packet += b'\x02'   #Message type: Boot Request (1)
+        packet += b'\x01'   #Hardware type: Ethernet
+        packet += b'\x06'   #Hardware address length: 6
+        packet += b'\x00'   #Hops: 0
+        packet += self.xid
+        packet += b'\x00\x00'    #Seconds elapsed: 0
+        packet += b'\x80\x00'    #Bootp flags: 0x8000 (Broadcast) + reserved flags
+        packet += inet_aton('0.0.0.0')
+        packet += inet_aton(self.offerIP)
+        packet += inet_aton(self.nextServerIP)
+        packet += inet_aton('0.0.0.0')
+        packet += self.mac
+        packet += b'\x00' * 10
+        packet += b'\x00' * 192
+        packet += b'\x63\x82\x53\x63' #Magic Cookie
+        packet += b'\x35\x01\x02'     # Message Type(Option 53 len=1 type=2(offer))
+        packet += b'\x01\x04' #subnet mask
+        packet += inet_aton(self.subnetMask) 
+        packet += b'\x03\x04' #router
+        packet += inet_aton(self.router) #router
+        packet += b'\x33\x04' #lease time
+        packet += convertBytes(self.leaseTime,4)
+        packet += b'\x36\x04' #DHCP server
+        packet += inet_aton(self.server)
+        packet += b'\x07\x04' #DNS servers
+        packet += inet_aton(self.DNS1)
+        packet +=  b'\x07\x04' #DNS servers
+        packet +=  inet_aton(self.DNS2)
+        packet += b'\x07\x04' #DNS servers
+        packet += inet_aton(self.DNS3)
+        
+        return bytes(packet)
 		
 class DHCPRequest:
-	def __init__(self, data):
-	    self.xid = b''
-	    self.mac = b''
-	    self.data = data
-	    self.unPack()
-	def unPack(self):
-	    print('********** Receive DHCP Request ***********')
-	    self.xid = data[4:8]
-	    self.mac = data[28:34]
-	    
-	    #print (self.xid)
-	    #print (self.mac)
+    def __init__(self, data):
+        self.xid = b''
+        self.mac = b''
+        self.data = data
+        self.unPack()
+    def unPack(self):
+        print('********** Receive DHCP Request ***********')
+        self.xid = data[4:8]
+        self.mac = data[28:34]
+        
+        #print (self.xid)
+        #print (self.mac)
 
 class DHCPAck:
-	def __init__(self, data, offerIP, nextServerIP, subnetMask, router, leaseTime, DHCPServer, DNS1, DNS2, DNS3):
-	    self.xid = data.xid
-	    self.mac = data.mac
-	    self.offerIP = offerIP
-	    self.nextServerIP = nextServerIP
-	    self.subnetMask = subnetMask
-	    self.router = router
-	    self.leaseTime = leaseTime
-	    self.server = DHCPServer
-	    self.DNS1 = DNS1
-	    self.DNS2 = DNS2
-	    self.DNS3 = DNS3
-	def sendPacket(self):
-	    packet = b''
-	    packet += b'\x02'   #Message type: Boot Request (1)
-	    packet += b'\x01'   #Hardware type: Ethernet
-	    packet += b'\x06'   #Hardware address length: 6
-	    packet += b'\x00'   #Hops: 0
-	    packet += self.xid
-	    packet += b'\x00\x00'    #Seconds elapsed: 0
-	    packet += b'\x80\x00'    #Bootp flags: 0x8000 (Broadcast) + reserved flags
-	    packet += inet_aton('0.0.0.0')
-	    packet += inet_aton(self.offerIP)
-	    packet += inet_aton(self.nextServerIP)
-	    packet += inet_aton('0.0.0.0')
-	    packet += self.mac
-	    packet += b'\x00' * 10
-	    packet += b'\x00' * 192
-	    packet += b'\x63\x82\x53\x63' #Magic Cookie
-	    packet += b'\x35\x01\x05'     # Message Type(Option 53 len=1 type=5(ACK))
-	    packet += b'\x01\x04' #subnet mask
-	    packet += inet_aton(self.subnetMask) 
-	    packet += b'\x03\x04' #router
-	    packet += inet_aton(self.router) #router
-	    packet += b'\x33\x04' #lease time
-	    packet += convertBytes(self.leaseTime,4)
-	    packet += b'\x36\x04' #DHCP server
-	    packet += inet_aton(self.server)
-	    packet += b'\x07\x04' #DNS servers
-	    packet += inet_aton(self.DNS1)
-	    packet +=  b'\x07\x04' #DNS servers
-	    packet +=  inet_aton(self.DNS2)
-	    packet += b'\x07\x04' #DNS servers
-	    packet += inet_aton(self.DNS3)
-	    return bytes(packet)
+    def __init__(self, data, offerIP, nextServerIP, subnetMask, router, leaseTime, DHCPServer, DNS1, DNS2, DNS3):
+        self.xid = data.xid
+        self.mac = data.mac
+        self.offerIP = offerIP
+        self.nextServerIP = nextServerIP
+        self.subnetMask = subnetMask
+        self.router = router
+        self.leaseTime = leaseTime
+        self.server = DHCPServer
+        self.DNS1 = DNS1
+        self.DNS2 = DNS2
+        self.DNS3 = DNS3
+    def sendPacket(self):
+        packet = b''
+        packet += b'\x02'   #Message type: Boot Request (1)
+        packet += b'\x01'   #Hardware type: Ethernet
+        packet += b'\x06'   #Hardware address length: 6
+        packet += b'\x00'   #Hops: 0
+        packet += self.xid
+        packet += b'\x00\x00'    #Seconds elapsed: 0
+        packet += b'\x80\x00'    #Bootp flags: 0x8000 (Broadcast) + reserved flags
+        packet += inet_aton('0.0.0.0')
+        packet += inet_aton(self.offerIP)
+        packet += inet_aton(self.nextServerIP)
+        packet += inet_aton('0.0.0.0')
+        packet += self.mac
+        packet += b'\x00' * 10
+        packet += b'\x00' * 192
+        packet += b'\x63\x82\x53\x63' #Magic Cookie
+        packet += b'\x35\x01\x05'     # Message Type(Option 53 len=1 type=5(ACK))
+        packet += b'\x01\x04' #subnet mask
+        packet += inet_aton(self.subnetMask) 
+        packet += b'\x03\x04' #router
+        packet += inet_aton(self.router) #router
+        packet += b'\x33\x04' #lease time
+        packet += convertBytes(self.leaseTime,4)
+        packet += b'\x36\x04' #DHCP server
+        packet += inet_aton(self.server)
+        packet += b'\x07\x04' #DNS servers
+        packet += inet_aton(self.DNS1)
+        packet +=  b'\x07\x04' #DNS servers
+        packet +=  inet_aton(self.DNS2)
+        packet += b'\x07\x04' #DNS servers
+        packet += inet_aton(self.DNS3)
+        return bytes(packet)
 
 if __name__ == "__main__":
     offerIP = '192.168.1.100'
